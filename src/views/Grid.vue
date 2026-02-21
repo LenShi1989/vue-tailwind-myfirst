7d65f3a94e02ce5245e3df57bdfacbf96681b04b
LenShi1989 <f0857gb@yahoo.com.tw>
2026-02-21 08:50:59 +0800

    feat: 更新 Grid.vue，將佈局從網格改為彈性佈局

--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="grid h-48 grid-cols-2 place-content-center gap-4 ...">
-    <div>01</div>
-    <div>02</div>
-    <div>03</div>
-    <div>04</div>
+  <div class="flex flex-wrap gap-4">
+    <div class="w-32 pt-6 bg-violet-400 rounded-lg">pt-6</div>
+    <div class="w-32 pr-4 bg-violet-400 rounded-lg">pr-4</div>
+    <div class="w-32 pb-8 bg-violet-400 rounded-lg">pb-8</div>
+    <div class="w-32 pl-2 bg-violet-400 rounded-lg">pl-2</div>
   </div>
 </template>
