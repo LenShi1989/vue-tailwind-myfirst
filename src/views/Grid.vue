4103a227ef418f90c2571733543cdc3f4a661843
LenShi1989 <f0857gb@yahoo.com.tw>
2026-03-09 01:45:08 +0800

    feat: 更新 Grid.vue，新增彈性佈局的樣式

--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -4,5 +4,18 @@
     <div class="w-32 pr-4 bg-violet-400 rounded-lg">pr-4</div>
     <div class="w-32 pb-8 bg-violet-400 rounded-lg">pb-8</div>
     <div class="w-32 pl-2 bg-violet-400 rounded-lg">pl-2</div>
+    <div class="w-32 px-8 bg-violet-400 rounded-lg">px-8</div>
+    <div class="w-32 py-8 bg-violet-400 rounded-lg">py-8</div>
+
+    <div dir="ltr">
+      <div class="w-32 ps-8 bg-violet-400 rounded-lg">ps-8</div>
+      <div class="w-32 pe-8 bg-violet-400 rounded-lg">pe-8</div>
+    </div>
+    <div dir="rtl">
+      <div class="w-32 ps-8 bg-violet-400 rounded-lg">ps-8</div>
+      <div class="w-32 pe-8 bg-violet-400 rounded-lg">pe-8</div>
+    </div>
+
+    <div class="m-8 bg-violet-400 rounded-lg">m-8</div>
   </div>
 </template>
