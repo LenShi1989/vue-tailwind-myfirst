f0ea89d7b8c20dcc07afe7e1e5a14cae3ab897e3
LenShi1989 <f0857gb@yahoo.com.tw>
2026-03-09 14:46:36 +0800

    feat: 更新 Grid.vue，新增邊距和對齊樣式

--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -17,5 +17,25 @@
     </div>
 
     <div class="m-8 bg-violet-400 rounded-lg">m-8</div>
+
+    <div class="mt-6 bg-violet-400 rounded-lg">mt-6</div>
+    <div class="mr-4 bg-violet-400 rounded-lg">mr-4</div>
+    <div class="mb-8 bg-violet-400 rounded-lg">mb-8</div>
+    <div class="ml-2 bg-violet-400 rounded-lg">ml-2</div>
+
+    <div class="mx-8 bg-violet-400 rounded-lg">mx-8</div>
+    <div class="my-8 bg-violet-400 rounded-lg">my-8</div>
+
+    <div class="h-16 w-36 bg-sky-400 opacity-20 bg-violet-400 rounded-lg"></div>
+    <div class="-mt-8 bg-sky-300 bg-violet-400 rounded-lg">-mt-8</div>
+
+    <div dir="ltr">
+      <div class="w-32 ms-8 bg-violet-400 rounded-lg">ms-8</div>
+      <div class="w-32 me-8 bg-violet-400 rounded-lg">me-8</div>
+    </div>
+    <div dir="rtl">
+      <div class="w-32 ms-8 bg-violet-400 rounded-lg">ms-8</div>
+      <div class="w-32 me-8 bg-violet-400 rounded-lg">me-8</div>
+    </div>
   </div>
 </template>
